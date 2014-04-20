e3972234ec68441030c2c3c58046b6f0de0daf97
Justyna Ilczuk <att@atte.ro>
2014-04-20 12:39:07 +0200

    Add docstrings to analytical module

--- hypergraph/analytical.py
+++ hypergraph/analytical.py
@@ -3,6 +3,19 @@ from collections import Counter
 
 
 def prediction(graph, model="hypergraph", plot_results=False):
+    """Predict diffusion on given graph using `model`.
+
+    Parameters:
+        graph: networkx graph or hypergraph
+        model: name of model used to compute prediction, currently
+        available models are:
+            - hypergraph
+            - clique
+        plot_results: should results be plotted in function?
+
+    Returns:
+        list of numbers representing probability of being in given node
+    """
     if model == "hypergraph":
         xs, ys = analytical_hypergraph_diffusion(graph)
         title = "Analytical prediction on diffusion on hypergraph"
@@ -16,14 +29,30 @@ def prediction(graph, model="hypergraph", plot_results=False):
     return ys
 
 
-def analytical_clique_diffusion(hypergraph, plot_results=False):
-    xs, ys = zip(*hypergraph.degree().items())
+def analytical_clique_diffusion(graph):
+    """Predict probabilities of being in a node on graph diffusion
+
+    Parameters:
+        graph: networkx graph, should have `degree` method implemented
+
+    Returns:
+        tuple of lists: nodes, probabilities
+    """
+    xs, ys = zip(*graph.degree().items())
     sum_of_ys = sum(ys)
     ys = [float(y) / sum_of_ys for y in ys]
     return xs, ys
 
 
 def analytical_hypergraph_diffusion(HG):
+    """Predict probabilities of being in a node in a hypergraph
+
+    Parameters:
+        HG: instance of `hypergraph`, should have `nodes` and `hyper_edges`
+            methods implemented
+
+    Predict using bipartite model from perspective of nodes.
+    """
     nodes = HG.nodes()
     hyper_edges = HG.hyper_edges()
     all_nodes = []
@@ -41,37 +70,3 @@ def analytical_hypergraph_diffusion(HG):
     ys = [float(y) / sum_of_ys for y in ys]
 
     return xs, ys
-
-
-def compute_C(hypergraph):
-    nodes = hypergraph.nodes()
-    hyper_edges = hypergraph.hyper_edges()
-
-    N = len(nodes)
-    M = len(hyper_edges)
-    C = np.zeros((N, M))
-    for i, node in enumerate(nodes):
-        for j, edge in enumerate(hyper_edges):
-            if node in edge:
-                C[i][j] = 1
-
-    for i in range(N):
-        if np.sum(C[i][:]):
-            C[i] /= np.sum(C[i][:])
-    return C
-
-def compute_D(hypergraph):
-    nodes = hypergraph.nodes()
-    hyper_edges = hypergraph.hyper_edges()
-
-    N = len(nodes)
-    M = len(hyper_edges)
-    D = np.zeros((M, N))
-    for i, edge in enumerate(hyper_edges):
-        for j, node in enumerate(nodes):
-            if node in edge:
-                D[i][j] = 1
-    for i in range(M):
-        if np.sum(D[i][:]):
-            D[i] /= np.sum(D[i][:])
-    return D
