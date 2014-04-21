7da670f55b4856dc56515be8c0a04569b66b9784
Justyna Ilczuk <att@atte.ro>
2014-04-21 22:41:19 +0200

    Add test of uniform generator

--- hypergraph/tests.py
+++ hypergraph/tests.py
@@ -1,5 +1,15 @@
+import nose.tools as nt
 from hypergraph.generators import uniform_hypergraph
 
 
 def test_uniform_generator():
-    pass
+    number_of_nodes = 10
+    number_of_edges = 5
+    cardinality = 3
+    hu = uniform_hypergraph(number_of_nodes, number_of_edges,
+                            cardinality)
+    nt.assert_true(hu)
+    nt.assert_equals(len(hu.nodes()), number_of_nodes)
+    nt.assert_equals(len(hu.hyper_edges()), number_of_edges)
+    nt.assert_true(all(len(e) == cardinality for e in hu.hyper_edges()))
+
