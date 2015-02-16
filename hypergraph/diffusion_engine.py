3b413326df830478bf139a4aa4c03ca97ed5ba78
Justyna Ilczuk <att@atte.ro>
2015-02-15 23:53:39 -0800

    Light refactor, update notebooks

--- hypergraph/diffusion_engine.py
+++ hypergraph/diffusion_engine.py
@@ -1,6 +1,4 @@
 import random
-import pickle
-import concurrent.futures
 
 from collections import Counter
 
@@ -11,36 +9,15 @@ class DiffusionEngine():
 
     Uses python futures to compute states concurrently.
     """
-    def __init__(self, markov_matrix, t_per_walker=None, max_walkers=None):
+    def __init__(self, markov_matrix):
         self.markov_matrix = markov_matrix
-        self.t_per_walker = t_per_walker or 10
-        self.max_walkers = max_walkers or 4
         self.available_steps = range(len(markov_matrix))
 
-    def simulate(self, t_max):
-        #TODO tests and refactor
-        number_of_walkers = max(t_max / self.t_per_walker, 1)
+    def simulate(self, t_max, current_state=0):
         all_states_per_iteration = []
         all_states = []
         c = Counter()
-        with concurrent.futures.ThreadPoolExecutor(max_workers=self.max_walkers) as executor:
-            futures = [executor.submit(simulate,
-                                       pickle.dumps(self.markov_matrix),
-                       random.choice(self.available_steps),
-                       self.t_per_walker) for _ in range(int(number_of_walkers))]
-            for future in concurrent.futures.as_completed(futures):
-                states = future.result()
-                all_states += states
-                all_states_per_iteration.append(states)
-        for state in all_states:
-            c[state] += 1
-        return c.most_common(), all_states_per_iteration
-
-    def simulate_plain(self, t_max, current_state=0):
-        all_states_per_iteration = []
-        all_states = []
-        c = Counter()
-        states = simulate(pickle.dumps(self.markov_matrix),
+        states = simulate(self.markov_matrix,
                           current_state,
                           t_max)
         all_states += states
@@ -57,8 +34,7 @@ def probabilities_to_distribution(discrete_probabilities):
     return [sum(discrete_probabilities[:i]) for i in range(1, len(discrete_probabilities) + 1)]
 
 
-def simulate(pickled_markov_matrix, current_state, t_max):
-    markov_matrix = pickle.loads(pickled_markov_matrix)
+def simulate(markov_matrix, current_state, t_max):
     state_to_distribution_function = {}
     for i, probabilities in enumerate(markov_matrix):
         state_to_distribution_function[i] = probabilities_to_distribution(probabilities)
